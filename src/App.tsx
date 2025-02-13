0f5946fc32d074d51f13e1e194ebc6de15419f9e
thantzintun <thantzintun.march201998@gmail.com>
2025-02-13 10:49:05 +0630

    fix_issue_two

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import { domain } from "./main";
 import { routeTree } from "./routeTree.gen";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 const router = createRouter({
   routeTree,
-  basepath: domain ? domain : "/",
+  basepath: "https://task-management-google-tasks-api.vercel.app",
 });
 
 declare module "@tanstack/react-router" {
